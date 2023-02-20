3976286c658348c42cc2dcd1c0ebf63c1d2d7349
SamTees <26529248+SamTees@users.noreply.github.com>
2023-02-20 22:19:40 +0000

    Fix fire ring not working in multiplayer

--- src/main/java/com/qrtstudios/wci/mixin/PlayerEntityMixin.java
+++ src/main/java/com/qrtstudios/wci/mixin/PlayerEntityMixin.java
@@ -1,25 +1,21 @@
 package com.qrtstudios.wci.mixin;
 
 import com.qrtstudios.wci.WhitelistCustomItems;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.particle.ParticleTypes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin({ClientPlayerEntity.class})
-public abstract class ClientPlayerEntityMixin {
-	@Shadow
-	public abstract boolean isUsingItem();
-
+@Mixin({PlayerEntity.class})
+public abstract class PlayerEntityMixin {
 	@Inject(method = "tick", at = @At("TAIL"))
 	void tick(CallbackInfo ci) {
 		LivingEntity entity = (LivingEntity) (Object) this;
 
-		if (this.isUsingItem() && entity.getActiveItem().isOf(WhitelistCustomItems.HELLFIRE_BOW)) {
+		if (entity.isUsingItem() && entity.getActiveItem().isOf(WhitelistCustomItems.HELLFIRE_BOW)) {
 			int i = entity.getItemUseTime() * 10;
 
 			double radians = Math.toRadians(i + 90);
